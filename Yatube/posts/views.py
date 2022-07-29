d101cf784ac426bbc57fd9f3a7d0b356f0da5e5e
Anton-1991128 <103500567+Anton-1991128@users.noreply.github.com>
2022-07-29 13:40:15 +0300

    CSS

--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = "Это главная страница проекта Yatube"
+    context = {
+        'title': title
+    }
+    return render(request, template, context) 
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Групповые посты {slug}')
+    template = 'posts/group_list.html'
+    info = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'info': info
+    }
+    return HttpResponse(f"Групповые посты {slug}", request, template, context)
