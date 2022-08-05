25dc342ac16a681e2a5fc65b5aa8deb7bc985659
Anton-1991128 <103500567+Anton-1991128@users.noreply.github.com>
2022-08-05 13:17:33 +0300

    First commit: change program.py

--- Yatube/posts/views.py
+++ Yatube/posts/views.py
@@ -1,20 +1,20 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from .models import Post, Group
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
-    template = 'posts/index.html'
-    title = "Это главная страница проекта Yatube"
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': title
+        'posts': posts,
     }
-    return render(request, template, context) 
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
-    info = "Здесь будет информация о группах проекта Yatube"
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'info': info
+        'group': group,
+        'posts': posts,
     }
-    return HttpResponse(f"Групповые посты {slug}", request, template, context)
+    return render(request, 'posts/group_list.html', context)
